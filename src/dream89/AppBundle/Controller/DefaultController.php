7b66c3a1f6672ce5846c29d8927231c9ec6063bd
Chanaka Sandaruwan <chanakasan@gmail.com>
2013-09-01 05:01:33 +0530

    Add excel format download, download links

--- src/dream89/AppBundle/Controller/DefaultController.php
+++ src/dream89/AppBundle/Controller/DefaultController.php
@@ -12,7 +12,7 @@ class DefaultController extends Controller
         return $this->render('dream89AppBundle:Default:index.html.twig');
     }
 
-    public function downloadAction()
+    public function downloadcsvAction()
     {
         $filename = 'products.csv';
         $path = $this->get('kernel')->getRootDir(). "/../reports/".$filename;
@@ -25,5 +25,39 @@ class DefaultController extends Controller
 
         $response->setContent($content);
         return $response;
+    }
+
+    public function downloadxlsAction()
+    {
+        $products = $this->_getProducts();
+        $filename = 'products.xls';
+        $content = $this->render(
+            'dream89AppBundle:Default:excel.xml.twig', array(
+                'products' => $products
+            ));
+
+        $response = new Response();
+        $response->headers->set('Content-Type', 'application/xls');
+        $response->headers->set('Content-Disposition', 'attachment;filename="'.$filename);
+
+        $response->setContent($content);
+        return $response;
+    }
+
+    private function _getProducts()
+    {
+        $products = array();
+        for($i=0; $i<10; $i++)
+        {
+            $product = new \StdClass();
+            $product->id = $i;
+            $product->name = 'Product '.$i;
+            $time = mktime(0,0,0,date("m"),date("d")-2*$i,date("Y"));
+            $product->released_on = date("Y/m/d", $time);
+            $product->price = 32.50+$i;
+            $products[] = $product;
+        }
+
+        return $products;
     }
 }
