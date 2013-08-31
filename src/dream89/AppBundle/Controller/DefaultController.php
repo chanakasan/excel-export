310c871ba3c3d1a969859e3b0ca38206a224acce
Chanaka Sandaruwan <chanakasan@gmail.com>
2013-08-31 20:09:47 +0530

    Add csv file download action

--- src/dream89/AppBundle/Controller/DefaultController.php
+++ src/dream89/AppBundle/Controller/DefaultController.php
@@ -3,11 +3,27 @@
 namespace dream89\AppBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class DefaultController extends Controller
 {
     public function indexAction()
     {
         return $this->render('dream89AppBundle:Default:index.html.twig');
+    }
+
+    public function downloadAction()
+    {
+        $filename = 'products.csv';
+        $path = $this->get('kernel')->getRootDir(). "/../reports/".$filename;
+        $content = file_get_contents($path);
+
+        $response = new Response();
+
+        $response->headers->set('Content-Type', 'text/csv');
+        $response->headers->set('Content-Disposition', 'attachment;filename="'.$filename);
+
+        $response->setContent($content);
+        return $response;
     }
 }
